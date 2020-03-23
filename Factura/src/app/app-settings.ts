b0565830bb0d12992cc9a56bfc603c159e6dd7ab
salomecodes <salomecodes@gmail.com>
2020-03-23 19:20:33 +0100

    added angular cors header

--- Factura/src/app/app-settings.ts
+++ Factura/src/app/app-settings.ts
@@ -5,7 +5,9 @@ export class AppSettings {
     public static INVOICE_SERVICE_ENDPOINT = environment.INVOICE_SERVICE_ENDPOINT;
 
     public static INVOICE_SERVICE_HEADER = { headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
       } 
     )};
 }
