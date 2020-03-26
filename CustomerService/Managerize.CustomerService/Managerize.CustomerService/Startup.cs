177560a39854927d02e55e6153b55b150a8dcbd3
salomecodes <salomecodes@gmail.com>
2020-03-26 17:15:14 +0100

    changed nginx config for ssl

--- CustomerService/Managerize.CustomerService/Managerize.CustomerService/Startup.cs
+++ CustomerService/Managerize.CustomerService/Managerize.CustomerService/Startup.cs
@@ -1,39 +1,26 @@
-using Managerize.InvoiceService.CompositionRoot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace InvoiceService
+namespace Managerize.CustomerService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
-
-            services.AddHttpsRedirection(option =>
-            {
-                option.HttpsPort = 4001;
-            });
-
-            services.AddControllers();
-            CompositionRoot.ConfigureServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,13 +28,12 @@ namespace InvoiceService
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
-            app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
