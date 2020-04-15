09c421df6c99f42dc565adea3a40747fc340ed56
SalomeCodes <salomecodes@gmail.com>
2020-04-15 08:52:58 +0200

    changed production environment variable

--- Factura/src/environments/environment.prod.ts
+++ Factura/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  INVOICE_SERVICE_ENDPOINT: window["env"]["INVOICE_SERVICE_ENDPOINT"] || "default",
+  INVOICE_SERVICE_ENDPOINT: "http://invoiceservice.daphneprojects.online",
   debug: window["env"]["debug"] || false
 };
